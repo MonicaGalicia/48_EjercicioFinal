2fb38da8dead60cf441e69c98874815090b1bc1d
MonicaGalicia <monica.garmz@gmail.com>
2016-12-06 17:22:41 +0000

    etapa 8

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,4 +1,20 @@
 $(document).ready( function(){
 	$(".js-menu").hide();
-	console.log("el documento esta listo!");
+
+	console.log("el documento esta listo");
+
+$('.js-show-recipe').click(function() {
+   $('div.page').addClass("make");
+   
 });
+
+$('.js-show-make').click(function() {
+    $('div.page').removeClass("make");
+    
+});
+
+});
+
+
+
+
